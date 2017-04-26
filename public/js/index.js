343f4d89229cb1f7a1699f71feaa89f6f05eabf3
Nofar Tamari <nofart@mycheck.co.il>
2017-04-26 14:52:54 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'client@example.com',
-    text: 'Client hey!'
-  });
 });
 
 socket.on('disconnect', function () {
