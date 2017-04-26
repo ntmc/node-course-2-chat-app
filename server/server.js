634e321b10fce9e6ccd4696c876a72a95876d485
Nofar Tamari <nofart@mycheck.co.il>
2017-04-26 14:37:36 +0300

    Create message events

--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'test@example.com',
+    text: 'Server Hey!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
